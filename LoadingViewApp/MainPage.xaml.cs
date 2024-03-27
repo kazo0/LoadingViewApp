3548d4fb6b2b293be86b3003637d49bc31a8d49d
Steve Bilogan <steve.bilogan@gmail.com>
2024-03-26 21:20:16 -0400

    first commit

---
+++ LoadingViewApp/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+namespace LoadingViewApp;
+
+public sealed partial class MainPage : Page
+{
+	public MainPage()
+	{
+		this.InitializeComponent();
+	}
+
+	private async void Start_Click(object sender, RoutedEventArgs e)
+	{
+	}
+	private async void Stop_Click(object sender, RoutedEventArgs e)
+	{
+	}
+}
